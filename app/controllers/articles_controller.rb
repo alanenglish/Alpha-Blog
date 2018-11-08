eeeaef16b28854f49159752a5c0008123c776d69
Alan English <aenglish@manorhillcapital.com>
2018-11-08 14:51:50 -0600

    create remaining CRUD operations, introduce partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,15 @@
 class ArticlesController < ApplicationController
-  def new
-    @article = Article.new
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
   end
 
   def create
@@ -16,10 +21,31 @@ class ArticlesController < ApplicationController
     else
       render :new
     end
+  end
 
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated."
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article.destroy
+    flash[:notice] = "Article was successfully deleted."
+    redirect_to articles_path
   end
 
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:title, :description)
