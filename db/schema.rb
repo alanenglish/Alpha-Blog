7fcf20a35b4ca4b3fb6109abab452ec086237e78
Alan English <aenglish@manorhillcapital.com>
2018-11-09 18:01:44 -0600

    create users table and model, add validations and associations with articles, add debug params if in development mode

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_08_001509) do
+ActiveRecord::Schema.define(version: 2018_11_09_234823) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
