500e0269e38ea06debed28938713c040cf9924e9
Alan English <aenglish@manorhillcapital.com>
2018-11-14 20:26:32 -0600

    add pagination gems and implement pagination on articles and users pages - create current_user, logged_in? and require_user in application controller as helper methods - create authenticate! method for both users and articles controllers - add authentication throughout the app and conditionally display features whether user is logged in or not - create sessions controller in order for user to login and logout - add custom sessions routes to config routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
+
+  # create all article actions
   resources :articles
+
+  # custom about route for pages controller
   get '/about' => 'pages#about'
-  resources :users, except: [:new]
+
+  # sign Up form to create new user
   get '/signup' => 'users#new'
+  # all other user actions except new (created custom /signup route above)
+  resources :users, except: [:new]
+
+  # custom login route for sessions new action - page displays login form
+  get '/login' => 'sessions#new'
+  # custom login route for sesseions create action - actually signs the user into platform
+  post '/login' => 'sessions#create'
+  # logout action
+  delete '/logout' => 'sessions#destroy'
 end
