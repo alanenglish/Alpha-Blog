e0afa2e40727cca0f066a2e78633ce4c38d2225e
Alan English <aenglish@manorhillcapital.com>
2018-11-27 12:24:30 -0600

    create article categories table, run migration, create article category model and set associations, update article and category models with has many associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
+  has_many  :article_categories
+  has_many  :articles, through: :article_categories
+
   validates :name, uniqueness: true, presence: true, length: { minimum: 3, maximum: 25}
 end
